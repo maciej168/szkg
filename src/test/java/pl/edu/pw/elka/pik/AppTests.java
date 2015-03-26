6c4e4ba1ea510c40ddb32c38edb1238e7dc56441
m168 <6865656+maciej168@users.noreply.github.com>
2015-03-26 19:37:01 +0100

    jUnit --> TestNG

--- src/test/java/pl/edu/pw/elka/pik/AppTests.java
+++ src/test/java/pl/edu/pw/elka/pik/AppTests.java
@@ -1,37 +1,36 @@
 package pl.edu.pw.elka.pik;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
-public class AppTests {
+public class  AppTests extends AbstractTestNGSpringContextTests {
     private MockMvc mockMvc;
 
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
     protected WebApplicationContext wac;
 
-    @Before
+    @BeforeTest
     public void setup() {
-        this.mockMvc = webAppContextSetup(this.wac).build();
+        //Autowired not available here yet
     }
 
     @Test
     public void simple() throws Exception {
+        this.mockMvc = webAppContextSetup(this.wac).build();
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("hello"));
