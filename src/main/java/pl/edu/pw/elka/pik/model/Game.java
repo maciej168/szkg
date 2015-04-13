ce5147aabf961258d8957d45348b99910bd56742
mcybuls1 <mikolaj_cybulski@hotmail.com>
2015-04-13 12:35:56 +0200

    Połączenie z Hibernate, z przykładową klasą

---
+++ src/main/java/pl/edu/pw/elka/pik/model/Game.java
@@ -0,0 +1,35 @@
+package pl.edu.pw.elka.pik.model;
+
+/**
+ * Created by Mikolaj on 2015-04-07.
+ */
+public class Game {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
